ed681195238a5eeff4536a593d31e18e902bfb99
phatdroid94@gmail.com <phatdroid94@gmail.com>
2020-03-17 18:42:34 +0700

    add code and xml

--- app/src/main/java/com/example/fragment26112019/MainActivity.java
+++ app/src/main/java/com/example/fragment26112019/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.fragment26112019;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mBtnAdd;
+    FragmentManager mFragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnAdd = findViewById(R.id.buttonAddFragment);
+        mFragmentManager = getSupportFragmentManager();
+
+        mBtnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AndroidFragment androidFragment = new AndroidFragment();
+                FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+                fragmentTransaction.add(R.id.linearContainer,androidFragment);
+                fragmentTransaction.commit();
+            }
+        });
     }
 }
